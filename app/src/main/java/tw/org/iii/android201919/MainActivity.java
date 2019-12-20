979527dbf5befd8bc0d2810bff10f7cb571d59d3
brad <brad@brad.tw>
2019-12-20 21:30:52 +0800

    10

--- app/src/main/java/tw/org/iii/android201919/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201919/MainActivity.java
@@ -6,24 +6,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.net.NetworkInterface;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager connectivityManager;
     private TelephonyManager telephonyManager;
+    private AccountManager accountManager;
     private MyReceiver myReceiver;
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +85,37 @@ public class MainActivity extends AppCompatActivity {
         Log.v("brad", telephonyManager.getMeid());
         Log.v("brad", telephonyManager.getSubscriberId());
 
+        accountManager = (AccountManager)getSystemService(Context.ACCOUNT_SERVICE);
+        Account[] accounts = accountManager.getAccounts();
+        for (Account account : accounts){
+            try {
+                String type = account.type;
+                String name = account.name;
+                String passwd = accountManager.getPassword(account);
+                Log.v("brad", type + ":" + name);
+            }catch (Exception e){
+
+            }
+        }
+
+        contentResolver =getContentResolver();
+
+        //====
+        Uri uri = CallLog.Calls.CONTENT_URI;    // SQLIte Table
+        Cursor c = contentResolver.query(uri, null,null,null, null);
+        while (c.moveToNext()){
+            String name = c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME));
+            String tel = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
+            String type= c.getString(c.getColumnIndex(CallLog.Calls.TYPE));
+            //String du = c.getString(c.getColumnIndex(CallLog.Calls.DURATION));
+            long date = c.getLong(c.getColumnIndex(CallLog.Calls.DATE));
+
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date cdate = new Date(date);
+            ;
+
+            Log.v("brad", name + ":" + tel + ":" + type + " => " + df.format(cdate));
+        }
 
     }
 
