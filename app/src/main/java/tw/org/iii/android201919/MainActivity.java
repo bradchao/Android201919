b3eac3d5f2cf2790f0c1e4ef3dd6609de42b03fc
brad <brad@brad.tw>
2019-12-20 20:35:09 +0800

    2

--- app/src/main/java/tw/org/iii/android201919/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201919/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.net.NetworkInterface;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager connectivityManager;
@@ -18,10 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         Log.v("brad", "network = " + isConnectNetwork());
+        Log.v("brad", "wifi = " + isWifiConnect());
     }
 
     private boolean isConnectNetwork(){
         NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        return info.isConnected();
+    }
+
+    private boolean isWifiConnect(){
+        NetworkInfo info = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return info.isConnected();
     }
 
